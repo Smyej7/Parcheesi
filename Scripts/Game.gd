0ed3dc7536129d3c89d232a9016ce9a387bf7093
Smyaj Hakim <smyaj.abdelhakim@gmail.com>
2021-11-26 21:25:37 +0100

    +++

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -31,6 +31,11 @@ onready var Players = get_node("Players")
 ####################################################
 #################### VARIABLESS ####################
 #var nouv_player
+var safezone = { "J" : [101, 102, 103, 104, 105, 106, 107, 108],
+				 "V" : [201, 202, 203, 204, 205, 206, 207, 208],
+				 "R" : [301, 302, 303, 304, 305, 306, 307, 308],
+				 "B" : [401, 402, 403, 404, 405, 406, 407, 408]}
+
 var current_player
 var players_list = ["J", "V", "R", "B"]
 var go_value
@@ -43,6 +48,9 @@ var y
 var rand1
 var rand2
 
+var game_over
+var old
+
 ################## GETTERS && SETTERS ##################
 func set_go_value(var value):
 	go_value = value
@@ -52,28 +60,86 @@ func get_go_value():
 ########################################################
 
 func _ready():
+	randomize()
 	go_value = 0
+	game_over = false
 	instance_texture_players()
 	start()
-	
-	rand1 = randi() % 6 + 1
-	rand2 = randi() % 6 + 1
-	
-	dice_texture(rand1, rand2)
+
+#func _ready():
+#	var tmp_last_move = ["jk", 12]
+#	print(tmp_last_move[0][0])
+##	print(safezone[tmp_last_move[0][0]].has(tmp_last_move[1]))
+
+func desac_pickable():
+	for p in Players.get_children():
+		if p.name != current_player:
+			for p0 in p.get_children():
+				p0.set_pickable(false)
+
+func activ_pickable():
+	for p in Players.get_children():
+		for p0 in p.get_children():
+			p0.set_pickable(true)
 
 func start():
-	randomize()
 	var rand_p = randi() % 4
-	current_player = players_list[rand_p]
+	old = rand_p
+	current_player = players_list[old]
+	desac_pickable()
+	tour_suiv()
+
+func tour_suiv():
+	which_button = 0
+	print("tour suiv")
+	get_node("Players").get_node(current_player).set_nbr_doubles(0)
+	get_node("Players").get_node(current_player).set_last_move("", 0)
+	activ_pickable()
+	old += 1
+	if (old > 3):
+		old = 0
+	current_player = players_list[old]
+	desac_pickable()
 	
-#	for p in Players.get_children():
-#		if p.name != current_player:
-#			for p0 in p.get_children():
-#				p0.set_pickable(false)
+	rnd()
+	
+	D1.disabled = false
+	D2.disabled = false
+
+func base_pleine() -> bool:
+	for i in range(1, 5):
+		if (BaseData.base_data["Base_" + current_player]["Pos" + str(i)]["val"] == 0):
+			return false
+	return true
+
+
+func rnd():
+	var tmp_nbr_doubles
+	var tmp_last_move
+#	rand1 = randi() % 6 + 1
+#	rand2 = randi() % 6 + 1
+	rand1 = 5
+	rand2 = 5
+	
+	dice_texture(rand1, rand2)
+	
+	
+	if (rand1 == rand2):
+		tmp_nbr_doubles = get_node("Players").get_node(current_player).get_nbr_doubles()
+		get_node("Players").get_node(current_player).set_nbr_doubles(tmp_nbr_doubles + 1)
+		if (get_node("Players").get_node(current_player).get_nbr_doubles() == 3):
+			tmp_last_move = get_node("Players").get_node(current_player).get_last_move()
+			if tmp_last_move[0] != "":
+				if (safezone[tmp_last_move[0][0]].has(tmp_last_move[1])):# protege
+					tour_suiv()
+				else:
+					get_node("Players").get_node(current_player).kill(tmp_last_move[0], tmp_last_move[1])
+					tour_suiv()
 	
 
-#func tour_suiv():
-	
+func relancer():
+	rnd()
+
 func dice_texture(var t1, var t2):
 	x = DiceData.diece_data["Dice_" + current_player]["x1"]
 	y = DiceData.diece_data["Dice_" + current_player]["y1"]
@@ -87,23 +153,25 @@ func dice_texture(var t1, var t2):
 	D2.icon = load("res://Assets/dieWhite_border" + str(t2) + ".png")
 
 func instance_texture_players():
+	
+	get_node("Players/J").set_nbr_doubles(0)
+	get_node("Players/V").set_nbr_doubles(0)
+	get_node("Players/R").set_nbr_doubles(0)
+	get_node("Players/B").set_nbr_doubles(0)
+	
 	var t
 	t = get_node("Players/J").get_children()
 	for p in t:
 		p.get_node("Sprite").set_texture(J_texture)
-		p.set_nbr_doubles(0)
 	t = get_node("Players/V").get_children()
 	for p in t:
 		p.get_node("Sprite").set_texture(V_texture)
-		p.set_nbr_doubles(0)
 	t = get_node("Players/R").get_children()
 	for p in t:
 		p.get_node("Sprite").set_texture(R_texture)
-		p.set_nbr_doubles(0)
 	t = get_node("Players/B").get_children()
 	for p in t:
 		p.get_node("Sprite").set_texture(B_texture)
-		p.set_nbr_doubles(0)
 	
 #var a = 23
 #func _on_Jaune2_input_event(viewport, event, shape_idx):
@@ -113,34 +181,44 @@ func instance_texture_players():
 #		#a += 23.2
 
 func _on_test_button_pressed():
-	
-	print("Item1 : ", CaseData.case_data["Case59"]["Item1"])
-	print("Item2 : ", CaseData.case_data["Case59"]["Item2"])
-	
-	print(BaseData.base_data["Base_R"]["Pos1"]["val"])
-	print(BaseData.base_data["Base_R"]["Pos2"]["val"])
-	print(BaseData.base_data["Base_R"]["Pos3"]["val"])
-	print(BaseData.base_data["Base_R"]["Pos4"]["val"])
-	
-#	print("#############")
-#	print("Item1 : ", CaseData.case_data["Case108"]["Item1"])
-#	print("Item2 : ", CaseData.case_data["Case108"]["Item2"])
-#	print("Item3 : ", CaseData.case_data["Case108"]["Item3"])
-#	print("Item4 : ", CaseData.case_data["Case108"]["Item4"])
+	print("Base J : " + str(BaseData.base_data["Base_J"]["Pos1"]["val"]) + " " + str(BaseData.base_data["Base_J"]["Pos2"]["val"]) + " " + str(BaseData.base_data["Base_J"]["Pos3"]["val"]) + " " + str(BaseData.base_data["Base_J"]["Pos4"]["val"]))
+	print("Base V : " + str(BaseData.base_data["Base_V"]["Pos1"]["val"]) + " " + str(BaseData.base_data["Base_V"]["Pos2"]["val"]) + " " + str(BaseData.base_data["Base_V"]["Pos3"]["val"]) + " " + str(BaseData.base_data["Base_V"]["Pos4"]["val"]))
+	print("Base R : " + str(BaseData.base_data["Base_R"]["Pos1"]["val"]) + " " + str(BaseData.base_data["Base_R"]["Pos2"]["val"]) + " " + str(BaseData.base_data["Base_R"]["Pos3"]["val"]) + " " + str(BaseData.base_data["Base_R"]["Pos4"]["val"]))
+	print("Base B : " + str(BaseData.base_data["Base_B"]["Pos1"]["val"]) + " " + str(BaseData.base_data["Base_B"]["Pos2"]["val"]) + " " + str(BaseData.base_data["Base_B"]["Pos3"]["val"]) + " " + str(BaseData.base_data["Base_B"]["Pos4"]["val"]))
+	print("----------------")
+	print("Item 1 : " + CaseData.case_data["Case22"]["Item1"])
+	print("Item 2 : " + CaseData.case_data["Case22"]["Item2"])
+	print(get_node("Players/B/B3").in_base)
 
+func D1_is_disabled() -> bool:
+	if (D1.disabled):
+		return true
+	else:
+		return false
+
+func D2_is_disabled() -> bool:
+	if (D2.disabled):
+		return true
+	else:
+		return false
 
 func _on_D1_pressed():
-	go_value = rand1
-	which_button = 1
-	print("D1 pressed")
+	if (go_value != 20):
+		go_value = rand1
+		which_button = 1
+		print("D1 pressed")
 
 
 func _on_D2_pressed():
-	go_value = rand2
-	which_button = 2
-	print("D2 pressed")
+	if (go_value != 20):
+		go_value = rand2
+		which_button = 2
+		print("D2 pressed")
 
 
 func _on_hhh_pressed():
 	print(go_value)
-	go_value = 3
+	go_value = 5
+
+func _on_AcceptDialog_confirmed():
+	get_tree().quit()
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -7,7 +7,6 @@ onready var p_dep = get_parent().point_dep
 onready var Game_node = get_parent().get_parent().get_parent()
 onready var Parent_node = get_parent()
 
-var nbr_doubles
 
 
 var x
@@ -16,41 +15,87 @@ var y
 var in_base = true
 
 ################## GETTERS && SETTERS ##################
-func set_nbr_doubles(var value):
-	nbr_doubles = value
-
-func get_nbr_doubles():
-	return nbr_doubles
 
 func set_in_base(var value):
 	in_base = value
+
+func get_case_pos():
+	return case_pos
 ########################################################
 
 func _ready():
 #	case_pos = p_dep
 	pass
 
+#func depart():
+#	var name0 = self.name
+#
+#	if(from_to(p_dep-1, p_dep)):
+#		BaseData.base_data["Base_" + name0[0]]["Pos" + name0[1]]["val"] = 0
+#		case_pos = p_dep
+#		c_pos = case_pos
+#		go_to(case_pos)
+#		in_base = false
+#		return true
+#	else:
+#		return false
+
+var go_to2 = false
+func qd():
+	case_pos = p_dep
+	c_pos = case_pos
+	go_to2 = true
+	go_to(case_pos)
+	in_base = false
+
 func depart():
 	var name0 = self.name
 	
-	if(from_to(p_dep-1, p_dep)):
+	if(CaseData.case_data["Case" + str(p_dep)]["Item1"] == "0" && CaseData.case_data["Case" + str(p_dep)]["Item2"] == "0"): # les 2 vides 
 		BaseData.base_data["Base_" + name0[0]]["Pos" + name0[1]]["val"] = 0
-#		if (CaseData.case_data["Case" + str(p_dep)]["Item1"] == "0"):
-#			CaseData.case_data["Case" + str(p_dep)]["Item1"] = self.name
-#		else:
-#			CaseData.case_data["Case" + str(p_dep)]["Item1"] = self.name
-		case_pos = p_dep
-		c_pos = case_pos
-		go_to(case_pos)
-		in_base = false
+		qd()
 		return true
+	elif (CaseData.case_data["Case" + str(p_dep)]["Item1"] != "0" && CaseData.case_data["Case" + str(p_dep)]["Item2"] == "0"): # 1 nn vide , 2 vide
+		BaseData.base_data["Base_" + name0[0]]["Pos" + name0[1]]["val"] = 0
+		qd()
+		return true
+	elif (CaseData.case_data["Case" + str(p_dep)]["Item1"] != "0" && CaseData.case_data["Case" + str(p_dep)]["Item2"] != "0"): # les 2 nn vides
+		if (CaseData.case_data["Case" + str(p_dep)]["Item1"][0] == Parent_node.name && CaseData.case_data["Case" + str(p_dep)]["Item2"][0] == Parent_node.name):
+			return false
+		else: # 1 ennemi minimum
+			var victime
+			BaseData.base_data["Base_" + name0[0]]["Pos" + name0[1]]["val"] = 0
+			if (CaseData.case_data["Case" + str(p_dep)]["Item2"][0] != Parent_node.name):
+				victime = CaseData.case_data["Case" + str(p_dep)]["Item2"]
+				print("lvictime howa : " + victime)
+				kill(victime)
+				CaseData.case_data["Case" + str(p_dep)]["Item2"] = self.name
+				Game_node.set_go_value(20)
+				print(Game_node.get_go_value())
+			else:
+				victime = CaseData.case_data["Case" + str(p_dep)]["Item1"]
+				print("lvictime howa : " + victime)
+				kill(victime)
+				CaseData.case_data["Case" + str(p_dep)]["Item1"] = self.name
+				Game_node.set_go_value(20)
+				print(Game_node.get_go_value())
+				
+			qd()
+			return true
 	else:
 		return false
+
+#var victime = CaseData.case_data["Case" + str(pos_)]["Item1"]
+#kill(victime)
 
 var temp_name1
 var temp_name2
 
 func go_to(pos_):
+	Parent_node.set_last_move(self.name, pos_)
+	if (!go_to2):
+		Game_node.set_go_value(0)
+	go_to2 = false
 	print("pos_ : ", pos_)
 	case_pos = pos_
 	var temp_name
@@ -80,6 +125,8 @@ func go_to(pos_):
 			if (CaseData.case_data["Case" + str(pos_)]["Item" + str(5-i)] == "0"):
 				CaseData.case_data["Case" + str(pos_)]["Item" + str(5-i)] = self.name
 				Game_node.get_node("Players").get_node(self.name[0]).get_node(self.name).position = Vector2(CaseData.case_data["Case" + str(pos_)]["x" + str(5-i)], CaseData.case_data["Case" + str(pos_)]["y" + str(5-i)])
+				if (i == 4):
+					Game_node.get_node("AcceptDialog").popup()
 				return
 	else:
 		x = CaseData.case_data["Case" + str(pos_)]["x"]
@@ -90,41 +137,39 @@ func go_to(pos_):
 		if (CaseData.case_data["Case" + str(pos_)]["Item1"] == "0"):
 			CaseData.case_data["Case" + str(pos_)]["Item1"] = self.name
 		else:
-			if (Game_node.safe_cases.has(pos_)):
+			if (Game_node.safe_cases.has(pos_) || CaseData.case_data["Case" + str(pos_)]["Item1"][0] == self.name[0]):
 				
 				CaseData.case_data["Case" + str(pos_)]["Item2"] = self.name
 				temp_name1 = CaseData.case_data["Case" + str(case_pos)]["Item1"]
 				temp_name2 = CaseData.case_data["Case" + str(case_pos)]["Item2"]
-				rect_pos(case_pos)
+				rect_pos(case_pos, temp_name1, temp_name2)
 			else:
 				var victime = CaseData.case_data["Case" + str(pos_)]["Item1"]
 				kill(victime)
 				CaseData.case_data["Case" + str(pos_)]["Item1"] = self.name
 				Game_node.set_go_value(20)
 				print(Game_node.get_go_value())
-	
-	print("land on Case ", pos_)
-	print("Item1 : ", CaseData.case_data["Case" + str(pos_)]["Item1"])
-	print("Item2 : ", CaseData.case_data["Case" + str(pos_)]["Item2"])
 
 func kill(var victime):
-	print("KILLED")
+	
+	print(victime + " /KILLED")
 	Game_node.get_node("Players").get_node(victime[0]).get_node(victime).set_in_base(true)
 	BaseData.base_data["Base_" + victime[0]]["Pos" + victime[1]]["val"] = 1
 	var x0 = BaseData.base_data["Base_" + victime[0]]["Pos" + victime[1]]["x"]
 	var y0 = BaseData.base_data["Base_" + victime[0]]["Pos" + victime[1]]["y"]
 	Game_node.get_node("Players").get_node(victime[0]).get_node(victime).position = Vector2(x0, y0)
 
-func rect_pos(var p):
+
+func rect_pos(var p, tmp_name1, tmp_name2):
 	var vect = Vector2(CaseData.case_data["Case" + str(p)]["x"], CaseData.case_data["Case" + str(p)]["y"])
 	if (p >= 60 && p <= 68) || (p >= 1 && p <= 8) || (p >= 26 && p <= 42) || (p >= 101 && p <= 107) || (p >= 301 && p <= 307):
-		Game_node.get_node("Players").get_node(str(temp_name1[0])).get_node(str(temp_name1)).position = vect + Vector2(-11, 0)
-		Game_node.get_node("Players").get_node(str(temp_name2[0])).get_node(str(temp_name2)).position = vect + Vector2(+11, 0)
-		print("pos rectifiée pour ( " , temp_name1, " ) && ( " , temp_name2, " )")
+		Game_node.get_node("Players").get_node(str(tmp_name1[0])).get_node(str(tmp_name1)).position = vect + Vector2(-11, 0)
+		Game_node.get_node("Players").get_node(str(tmp_name2[0])).get_node(str(tmp_name2)).position = vect + Vector2(+11, 0)
+		print("pos rectifiée pour ( " , tmp_name1, " ) && ( " , tmp_name2, " )")
 	elif (p >= 9 && p <= 25) || (p >= 43 && p <= 59) || (p >= 201 && p <= 207) || (p >= 401 && p <= 407):
-		Game_node.get_node("Players").get_node(str(temp_name1[0])).get_node(str(temp_name1)).position = vect + Vector2(0, +11)
-		Game_node.get_node("Players").get_node(str(temp_name2[0])).get_node(str(temp_name2)).position = vect + Vector2(0, -11)
-		print("pos rectifiée pour ( " , temp_name1, " ) && ( " , temp_name2, " )")
+		Game_node.get_node("Players").get_node(str(tmp_name1[0])).get_node(str(tmp_name1)).position = vect + Vector2(0, +11)
+		Game_node.get_node("Players").get_node(str(tmp_name2[0])).get_node(str(tmp_name2)).position = vect + Vector2(0, -11)
+		print("pos rectifiée pour ( " , tmp_name1, " ) && ( " , tmp_name2, " )")
 	else:
 		print("-------------------------------------------------------")
 
@@ -155,6 +200,8 @@ func avancer(nbr):
 				return false
 		elif (case_pos <= 68 && next_pos > 68):# mn bra l dakhel
 			next_pos = 100 + next_pos - 68
+			if (next_pos > 108):
+				return false
 			if (from_to(case_pos, 68)):# lpiste li bra wach clean
 				if (from_to(100, next_pos)):# lpiste li ldakhel wach clean
 					go_to(next_pos)
@@ -192,6 +239,8 @@ func avancer(nbr):
 				return false
 		elif(case_pos <= 51 && next_pos > 51):# mn bra ldakhel
 			next_pos = 200 + next_pos - 51
+			if (next_pos > 208):
+				return false
 			if (from_to(case_pos, 51)):# lpiste li bra wach clean
 				if (from_to(200, next_pos)):# lpiste li ldakhel wach clean
 					go_to(next_pos)
@@ -229,6 +278,8 @@ func avancer(nbr):
 				return false
 		elif(case_pos <= 34 && next_pos > 34):# mn bra ldakhel
 			next_pos = 300 + next_pos - 34
+			if (next_pos > 308):
+				return false
 			if (from_to(case_pos, 34)):# lpiste li bra wach clean
 				if (from_to(300, next_pos)):# lpiste li ldakhel wach clean
 					go_to(next_pos)
@@ -266,6 +317,8 @@ func avancer(nbr):
 				return false
 		elif(case_pos <= 17 && next_pos > 17):# mn bra ldakhel
 			next_pos = 400 + next_pos - 17
+			if (next_pos > 408):
+				return false
 			if (from_to(case_pos, 17)):# lpiste li bra wach clean
 				if (from_to(400, next_pos)):# lpiste li ldakhel wach clean
 					go_to(next_pos)
@@ -296,12 +349,21 @@ func _on_Player_input_event(viewport, event, shape_idx):
 			if (Game_node.go_value == 5):
 				var testd = depart()
 				if (testd):
-					Game_node.go_value = 0
+					if (Game_node.go_value != 20):
+						Game_node.go_value = 0
 					desac_button()
 		elif(Game_node.go_value != 0):
 			var test = avancer(Game_node.go_value)
 			print("go_value // ", Game_node.go_value)
 			if (test):
-				Game_node.go_value = 0
+				if (Game_node.go_value != 20):
+					Game_node.go_value = 0
 				desac_button()
 			print("test // ", test)
+		#tour suiv ou pas
+	if((Game_node.rand1 == Game_node.rand2) && Game_node.get_go_value() != 20 && Game_node.D1_is_disabled() && Game_node.D2_is_disabled()):
+		Game_node.D1.disabled = false
+		Game_node.D2.disabled = false
+		Game_node.relancer()
+	elif ((Game_node.rand1 != Game_node.rand2) && Game_node.get_go_value() != 20 && Game_node.D1_is_disabled() && Game_node.D2_is_disabled()):
+		Game_node.tour_suiv()
